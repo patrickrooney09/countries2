c1216a59fb8bd0a7c8fda621435c774d2f21ed76
Patrick Rooney <patrickrooney09@gmail.com>
2024-01-19 18:52:30 -0500

    added weather api, hid api key in env file

--- src/components/SingleCountryDetailed.jsx
+++ src/components/SingleCountryDetailed.jsx
@@ -1,5 +1,23 @@
+import countryService from "../services/countries";
+import { useEffect, useState } from "react";
+
 function SingleCountryDetailed(props) {
   const { country } = props;
+
+  const [temp, setTemp] = useState(0);
+
+  useEffect(() => {
+    countryService
+      .getCapitalWeather(
+        country.capitalInfo.latlng[0],
+        country.capitalInfo.latlng[1]
+      )
+      .then((response) => {
+        console.log(response.data);
+        setTemp(response.data.current.temp);
+      });
+  }, []);
+
   return (
     <li>
       <div>
@@ -13,11 +31,19 @@ function SingleCountryDetailed(props) {
           <strong>Languages:</strong>
           <ul>
             {Object.keys(country.languages).map((currentLanguage, index) => {
-              return <li key={index}>{country.languages[currentLanguage]}</li>;
+              return (
+                <li key={index}>
+                  <strong>{country.languages[currentLanguage]}</strong>
+                </li>
+              );
             })}
           </ul>
         </div>
         <img src={country.flags.png} />
+        <div>
+          <h3>Weather in {country.capital[0]}</h3>
+          <div>{temp} Farenheit</div>
+        </div>
       </div>
     </li>
   );
--- src/services/countries.js
+++ src/services/countries.js
@@ -1,10 +1,18 @@
 import axios from "axios";
 const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api/all";
+const weatherAPI = import.meta.env.VITE_API_KEY;
 
 const getAll = () => {
   return axios.get(baseUrl);
 };
 
+const getCapitalWeather = (latitude, longitude) => {
+  return axios.get(
+    `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&units=imperial&appid=${weatherAPI}`
+  );
+};
+
 export default {
   getAll,
+  getCapitalWeather,
 };
